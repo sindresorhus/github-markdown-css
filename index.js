688be4daf65f08fdfbf6a58955a628ae63b6ec49
Sindre Sorhus <sindresorhus@gmail.com>
2014-06-22 19:10:18 +0200

    bump deps and refactor code

--- index.js
+++ index.js
@@ -43,43 +43,45 @@ function cleanupCss(str) {
 	var mdBodyProps = [];
 
 	style.stylesheet.rules = style.stylesheet.rules.filter(function (el) {
-		if (el.type === 'keyframes' || el.type === 'comment') {
+		if (el.type === 'keyframes' || el.type === 'comment' || el.type === 'font-face') {
 			return false;
 		}
 
-		if (el.type === 'rule' && /::-webkit-validation|:-moz-placeholder|^\.integrations-slide-content|^\.prose-diff|@font-face|^\.octicon|^button::|^\.markdown-body .+(:hover|\.octicon)|^article$/.test(el.selectors[0])) {
-			return false;
-		}
+		if (el.type ==='rule') {
+			if (/::-webkit-validation|:-moz-placeholder|^\.integrations-slide-content|^\.prose-diff|@font-face|^\.octicon|^button::|^\.markdown-body .+(:hover|\.octicon)|^article$/.test(el.selectors[0])) {
+				return false;
+			}
 
-		if (el.selectors.length === 1 && /^(?:html|body)$/.test(el.selectors[0])) {
-			el.declarations = el.declarations.filter(function (declaration) {
-				if (!/^font|^(?:line-height|color)$|text-size-adjust$/.test(declaration.property)) {
-					return false;
+			if (el.selectors.length === 1 && /^(?:html|body)$/.test(el.selectors[0])) {
+				el.declarations = el.declarations.filter(function (declaration) {
+					if (!/^font|^(?:line-height|color)$|text-size-adjust$/.test(declaration.property)) {
+						return false;
+					}
+
+					return true;
+				});
+			}
+
+			el.selectors = el.selectors.map(function (selector) {
+				if (/^(?:body|html)$/.test(selector)) {
+					selector = '.markdown-body';
 				}
 
-				return true;
+				if (!/\.markdown-body/.test(selector)) {
+					selector = '.markdown-body ' + selector;
+				}
+
+				return selector;
 			});
+
+			// collect `.markdown-body` rules
+			if (el.selectors.length === 1 && el.selectors[0] === '.markdown-body') {
+				[].push.apply(mdBodyProps, el.declarations);
+				return false;
+			}
 		}
-
-		el.selectors = el.selectors.map(function (selector) {
-			if (/^(?:body|html)$/.test(selector)) {
-				selector = '.markdown-body';
-			}
-
-			if (!/\.markdown-body/.test(selector)) {
-				selector = '.markdown-body ' + selector;
-			}
-
-			return selector;
-		});
 
 		if (el.declarations.length === 0) {
-			return false;
-		}
-
-		// collect `.markdown-body` rules
-		if (el.selectors.length === 1 && el.selectors[0] === '.markdown-body') {
-			[].push.apply(mdBodyProps, el.declarations);
 			return false;
 		}
 
